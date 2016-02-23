4b64057dbba2c15d18a7550c053e69b2ad6d4648
Malathi <malathim993@gmail.com>
2016-02-23 11:52:42 +0530

    java:1.8_created

---
+++ src/main/java/io/swagger/configuration/WebMvcConfiguration.java
@@ -0,0 +1,12 @@
+package io.swagger.configuration;
+
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-02-23T06:22:27.012Z")
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
